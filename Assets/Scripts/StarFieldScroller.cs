125de27f230c01bebef4ac27a5ca1af2f1a7b495
Majid <majidted@gmail.com>
2021-04-30 16:31:37 +0430

    feat: star field

---
+++ Assets/Scripts/StarFieldScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarFieldScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, scrollSpeed * Time.deltaTime, 0);
+        if(transform.position.y < -10.0f) {
+            transform.Translate(0, 20.48f, 0);
+        }
+    }
+}
